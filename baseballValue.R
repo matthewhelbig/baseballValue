8e610c5807be606de967c209a0a22cb0f6d1f30f
Matthew Helbig <helbig.matthew@gmail.com>
2019-09-30 22:30:52 -0400

    Committing everything at end of day.

--- baseballValue.R
+++ baseballValue.R
@@ -1,5 +1,14 @@
-library(RMySQL)
 library(dplyr)
 
-mydb = dbConnect(MySQL(), user='root', password='doorknob18pillow', dbname='Lahman2016', host='127.0.0.1')
-dbListTables(mydb)
+#Read in data and view the head of the data briefly
+Daily_bat <- read.csv("war_daily_bat.csv")
+Daily_bat %>% select(1:10) %>% slice(1:3)
+
+kept_Columns = select(Daily_bat, 1, 2, 4, 5, 6, 31, 32, 33, 36, 47)
+years_pitchers_Subset <- subset(kept_Columns, 2005 < year_ID & year_ID < 2017 & pitcher == "N")
+Batting_value <- select(years_pitchers_Subset, -9)
+
+write.csv(Batting_value, "Batting_Value.csv")
+
+trout <- subset(Batting_value, player_ID == "troutmi01")
+View(trout)
