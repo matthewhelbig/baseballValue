4f7980330654db446a344fc23342d794db2ffa22
Matthew Helbig <helbig.matthew@gmail.com>
2019-10-02 22:38:38 -0400

    Committing at end of day.

--- baseballValue.R
+++ baseballValue.R
@@ -11,33 +11,23 @@ Batting_value <- select(years_pitchers_Subset, -9)
 write.csv(Batting_value, "Batting_Value.csv")
 
 Pre_Value_Salary <- read.csv("Value_Salary.csv")
-#Create year subsets
 
-oh_six <- subset(Pre_Value_Salary, yearID == 2006)
-oh_seven <- subset(Pre_Value_Salary, yearID == 2007)
-oh_eight <- subset(Pre_Value_Salary, yearID == 2008)
-oh_nine <- subset(Pre_Value_Salary, yearID == 2009)
-ten <- subset(Pre_Value_Salary, yearID == 2010)
-eleven <- subset(Pre_Value_Salary, yearID == 2011)
-twelve <- subset(Pre_Value_Salary, yearID == 2012)
-thirteen <- subset(Pre_Value_Salary, yearID == 2013)
-fourteen <- subset(Pre_Value_Salary, yearID == 2014)
-fifteen <- subset(Pre_Value_Salary, yearID == 2015)
-sixteen <- subset(Pre_Value_Salary, yearID == 2016)
+#split function to create subsets by year
+year <- split(Pre_Value_Salary, Pre_Value_Salary$yearID)
 
 #Eliminate duplicates
 
-dup6 <- oh_six[!duplicated(oh_six$playerID),]
-dup7 <- oh_seven[!duplicated(oh_seven$playerID),]
-dup8 <- oh_eight[!duplicated(oh_eight$playerID),]
-dup9 <- oh_nine[!duplicated(oh_nine$playerID),]
-dup10 <- ten[!duplicated(ten$playerID),]
-dup11 <- eleven[!duplicated(eleven$playerID),]
-dup12 <- twelve[!duplicated(twelve$playerID),]
-dup13 <- thirteen[!duplicated(thirteen$playerID),]
-dup14 <- fourteen[!duplicated(fourteen$playerID),]
-dup15 <- fifteen[!duplicated(fifteen$playerID),]
-dup16 <- sixteen[!duplicated(sixteen$playerID),]
+dup6 <- year$`2006`[!duplicated(year$`2006`$playerID),]
+dup7 <- year$`2007`[!duplicated(year$`2007`$playerID),]
+dup8 <- year$`2008`[!duplicated(year$`2008`$playerID),]
+dup9 <- year$`2009`[!duplicated(year$`2009`$playerID),]
+dup10 <- year$`2010`[!duplicated(year$`2010`$playerID),]
+dup11 <- year$`2011`[!duplicated(year$`2011`$playerID),]
+dup12 <- year$`2012`[!duplicated(year$`2012`$playerID),]
+dup13 <- year$`2013`[!duplicated(year$`2013`$playerID),]
+dup14 <- year$`2014`[!duplicated(year$`2014`$playerID),]
+dup15 <- year$`2015`[!duplicated(year$`2015`$playerID),]
+dup16 <- year$`2016`[!duplicated(year$`2016`$playerID),]
 
 #Bind them back together
 
